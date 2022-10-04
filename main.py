5764973ae07734c2e1486004ab4ec677cee6d2be
Ougi <demon_284@bk.ru>
2022-10-04 21:50:39 +0500

    Убрали лишние комментарии

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hi, {name}',1)  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 def new_func(func):
@@ -19,9 +16,8 @@ def new_func(func):
     return wrappler
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+
+
 
 
 @new_func
@@ -31,4 +27,4 @@ def test(a, b):
 
 test(4, 5)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
